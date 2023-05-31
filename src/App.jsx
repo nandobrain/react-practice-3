533f7b4684458365cdd8631045101c3c16c6b7f1
nandobrain <prf.arturo@gmail.com>
2023-05-31 13:53:40 -0700

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ import Axios from "axios";
 
 
 function App() {
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
 
   function fetchData() {
     Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      console.log(res.data)
+      setPredictedAge(res.data.age)
     });
   }
   return (
     <div className="App">
       <input placeHolder="Ex. Nando..." onChange={(event) => {setName(event.target.value)}}/>
       <button onClick={fetchData}> Predict Age </button>
+      <h1>Name:{predictedAge?.name}</h1>
+      <h1>Predicted Age:{predictedAge?.age}</h1>
+      <h1>Count:{predictedAge?.count}</h1>
     </div>
   )
 
